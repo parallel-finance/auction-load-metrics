c2de480295a131d1953f7dc85b2428e207ffd521
Alan WANG <alan@parallel.fi>
2021-11-11 22:57:36 +0800

    Change endpoint to polkadot

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,12 +1,12 @@
 import { SubstrateBlock, SubstrateEvent, SubstrateExtrinsic } from "@subql/types";
 import { ContributionSummary } from "../types";
 
-// const MULTISIG_ADDR = [
-//   "13wNbioJt44NKrcQ5ZUrshJqP7TKzQbzZt5nhkeL4joa3PAX",
-//   "12of6J5x9TyCo1qFn96ZFBqKTZd3Su6Ugy6qZbfRfyv3ktSU",
-// ];
+const MULTISIG_ADDR = [
+  "13wNbioJt44NKrcQ5ZUrshJqP7TKzQbzZt5nhkeL4joa3PAX",
+  "12of6J5x9TyCo1qFn96ZFBqKTZd3Su6Ugy6qZbfRfyv3ktSU",
+];
 
-const MULTISIG_ADDR = ["14auZo7SiRJUjnWoQdQJkQEFdy6KRF7xnyWRj91mxhPZKN4F"];
+// const MULTISIG_ADDR = ["14auZo7SiRJUjnWoQdQJkQEFdy6KRF7xnyWRj91mxhPZKN4F"];
 
 export const handleCrowdloanContribute = async ({ event, idx }: SubstrateEvent) => {
   const [who, fund, amount] = event.data.toArray();
