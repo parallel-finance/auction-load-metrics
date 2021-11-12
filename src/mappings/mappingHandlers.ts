e1108380001b97fb2bd5c63fddd2495e44442724
Alan WANG <alan@parallel.fi>
2021-11-12 15:33:29 +0800

    Fix summary bug

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -8,10 +8,12 @@ import { ContributionSummary } from "../types";
 
 const MULTISIG_ADDR = ["14auZo7SiRJUjnWoQdQJkQEFdy6KRF7xnyWRj91mxhPZKN4F"];
 
+const firstOrNull = (arr: any[]) => (arr.length > 0 ? arr[1] : null);
+
 export const handleCrowdloanContribute = async ({ event, idx }: SubstrateEvent) => {
   const [who, fund, amount] = event.data.toArray();
 
-  let record = await ContributionSummary.get(fund.toString());
+  let record = firstOrNull(await ContributionSummary.getByParaId(fund.toString()));
   if (record && record.account === who.toString()) {
     record.amount = (BigInt(record.amount) + BigInt(amount.toString())).toString();
   } else {
