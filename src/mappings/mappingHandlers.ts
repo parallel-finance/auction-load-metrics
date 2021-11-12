61e67854d4f30ddbe6897ca359384d70d888a088
Alan WANG <alan@parallel.fi>
2021-11-12 21:38:38 +0800

    Use moonbeam

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,8 +1,8 @@
 import { SubstrateBlock, SubstrateEvent, SubstrateExtrinsic } from "@subql/types";
 import { ContributionSummary } from "../types";
 
-const MULTISIG_ADDR = "13wNbioJt44NKrcQ5ZUrshJqP7TKzQbzZt5nhkeL4joa3PAX";
-// const MULTISIG_ADDR = "14auZo7SiRJUjnWoQdQJkQEFdy6KRF7xnyWRj91mxhPZKN4F";
+// const MULTISIG_ADDR = "13wNbioJt44NKrcQ5ZUrshJqP7TKzQbzZt5nhkeL4joa3PAX";
+const MULTISIG_ADDR = "14auZo7SiRJUjnWoQdQJkQEFdy6KRF7xnyWRj91mxhPZKN4F";
 
 export const handleCrowdloanContribute = async ({ event, idx }: SubstrateEvent) => {
   const [who, fund, amount] = event.data.toArray();
